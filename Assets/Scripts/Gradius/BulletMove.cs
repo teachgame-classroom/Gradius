14e05ce29b9c7e3ff8ae50347903f6306e337db5
teachgame <teachgame@163.com>
2019-04-05 12:23:24 +0800

    地面敌人

--- Assets/Scripts/Gradius/BulletMove.cs
+++ Assets/Scripts/Gradius/BulletMove.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class BulletMove : MonoBehaviour
 {
     public float speed = 15;
+    public Vector3 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(moveDirection == Vector3.zero)
+        {
+            moveDirection = transform.right;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);        
+        transform.Translate(moveDirection * speed * Time.deltaTime);        
     }
 }
--- Assets/Scripts/Gradius/Enemy.cs
+++ Assets/Scripts/Gradius/Enemy.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MovePattern { Straight, ZigZag, Sine }
+public enum MovePattern { Straight, ZigZag, Sine, Static }
+
+public enum MoveDirection { Left, Right, Up, Down }
+
 
 public class Enemy : MonoBehaviour
 {
     public MovePattern movePattern;
+
+    public MoveDirection straightMoveDirection;
+    public float straightMoveDistance = 0;
+
+    private float straightMoveTotalDistance = 0;
+
     public float speed = 10;
     public float changeDirectionPeriod = 1f;
     public float sineAmp = 1f;
@@ -26,10 +35,34 @@ public class Enemy : MonoBehaviour
     private List<Vector3> tracks = new List<Vector3>();
     private float lastRecordTime = 0;
 
+    private GameObject player;
+
+    public Sprite[] turrentSprites;
+    private SpriteRenderer spriteRenderer;
+    private Transform shotPos;
+
+    private Animator anim;
+
+    public GameObject bulletPrefab;
+    public float fireInterval;
+    private float lastFireTime;
+
     // Start is called before the first frame update
     void Start()
     {
         explosionPrefab = Resources.Load<GameObject>("Prefabs/Effects/Explosion_Red");
+
+        player = GameObject.Find("Vic Viper");
+
+        shotPos = transform.Find("ShotPos");
+
+        if(shotPos == null)
+        {
+            shotPos = transform;
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -49,6 +82,9 @@ public class Enemy : MonoBehaviour
                 case MovePattern.Sine:
                     SineMove();
                     break;
+                case MovePattern.Static:
+                    StaticMove();
+                    break;
             }
 
             if(Time.time - lastRecordTime > 0.1f)
@@ -61,11 +97,55 @@ public class Enemy : MonoBehaviour
                 lastRecordTime = Time.time;
             }
         }
+
+        if(bulletPrefab != null)
+        {
+            Shoot();
+        }
+    }
+
+    void StaticMove()
+    {
+        Vector3 aimDirection = GetAimDirection(player.transform.position);
+        SetSpriteByAimDirection(aimDirection);
     }
 
     void StraightMove()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        if(straightMoveDistance > 0)
+        {
+            straightMoveTotalDistance += speed * Time.deltaTime;
+
+            if(straightMoveTotalDistance < straightMoveDistance)
+            {
+                transform.Translate(GetStraightMoveDirection() * speed * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                anim.SetBool("Stop", true);
+            }
+        }
+        else
+        {
+            transform.Translate(GetStraightMoveDirection() * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    Vector3 GetStraightMoveDirection()
+    {
+        switch(straightMoveDirection)
+        {
+            case MoveDirection.Left:
+                return Vector3.left;
+            case MoveDirection.Right:
+                return Vector3.right;
+            case MoveDirection.Up:
+                return Vector3.up;
+            case MoveDirection.Down:
+                return Vector3.down;
+            default:
+                return Vector3.right;
+        }
     }
 
     void ZigSawMove()
@@ -92,6 +172,34 @@ public class Enemy : MonoBehaviour
         transform.Translate(velocity * Time.deltaTime, Space.World);
     }
 
+    Vector3 GetAimDirection(Vector3 targetPosition)
+    {
+        Vector3 aimDirection = (targetPosition - shotPos.position).normalized;
+        return aimDirection;
+    }
+
+    void SetSpriteByAimDirection(Vector3 aimDirection)
+    {
+        float angle = Vector3.Angle(Vector3.right, aimDirection) + 7.5f;
+
+        int spriteIdx = Mathf.FloorToInt(angle / 15);
+        Debug.Log("Aim Angle:" + angle + ", idx:" + spriteIdx);
+
+        spriteRenderer.sprite = turrentSprites[spriteIdx];
+
+    }
+
+    public void Shoot()
+    {
+        if(Time.time - lastFireTime > fireInterval)
+        {
+            GameObject bulletInstance = Instantiate(bulletPrefab, shotPos.position, Quaternion.identity);
+            bulletInstance.GetComponent<BulletMove>().moveDirection = GetAimDirection(player.transform.position);
+
+            lastFireTime = Time.time;
+        }
+    }
+
     public void Hurt(int damage)
     {
         hp -= damage;
@@ -104,18 +212,28 @@ public class Enemy : MonoBehaviour
 
     public void Die()
     {
-        squadonManager.OnMemberDestroy(transform.position);
+        if(squadonManager != null)
+        {
+            squadonManager.OnMemberDestroy(transform.position);
+        }
+
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = trackColor;
+        //Gizmos.color = trackColor;
 
-        for(int i = 0; i < tracks.Count; i++)
-        {
-            Gizmos.DrawSphere(tracks[i], 0.1f);
-        }
+        //for(int i = 0; i < tracks.Count; i++)
+        //{
+        //    Gizmos.DrawSphere(tracks[i], 0.1f);
+        //}
+
+        //if(shotPos != null && player != null)
+        //{
+        //    Gizmos.color = Color.white;
+        //    Gizmos.DrawLine(shotPos.position, shotPos.position + GetAimDirection(player.transform.position) * 5);
+        //}
     }
 }
